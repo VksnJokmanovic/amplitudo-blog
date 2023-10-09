23abbd331e14a02b692da09a5dbfdcbcf9c6d8d7
Vullet <104219958+VksnJokmanovic@users.noreply.github.com>
2023-10-09 15:47:34 +0200

    Work started
    9/10/2023-
    Added migration files for the post table and its appropriate controller + routes

--- blog-app/routes/web.php
+++ blog-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Display a list of all posts
+Route::get('/post', [postController::class, 'index'])->name('post.index');
+
+// Display a specific post by its ID
+Route::get('/post/{id}', [postController::class, 'show'])->name('post.show');
+
+// Display a form for creating a new post
+Route::get('/post/create', [postController::class, 'create'])->name('post.create');
+
+// Store a new post post in the database
+Route::post('/post', [postController::class, 'store'])->name('post.store');
+
+// Display a form for editing an existingpost
+Route::get('/post/{id}/edit', [postController::class, 'edit'])->name('post.edit');
+
+// Update an existing post in the database
+Route::put('/post/{id}', [postController::class, 'update'])->name('post.update');
