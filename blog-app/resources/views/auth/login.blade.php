71022c80ee92ed50537f126212f2e9ff68804425
Vullet <104219958+VksnJokmanovic@users.noreply.github.com>
2023-10-18 12:06:14 +0200

    Update login.blade.php
    I don't know how I missed this, but after some tweaking the app now has a functioning login view.

---
+++ blog-app/resources/views/auth/login.blade.php
@@ -0,0 +1,35 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus  />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+
+
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Register') }}
+            </a>
+
+{{--            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">--}}
+{{--                {{ __('Forgotten password?') }}--}}
+{{--            </a>--}}
+            <x-primary-button class="ml-4">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
